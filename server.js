227bfc22a7e17b580016a6b233f129afd5e21283
Marcellinom <marcellino.janitra@yahoo.com>
2024-05-13 15:11:02 +0700

    optimize server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-const port = 3000
 const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser');
 const path = require('path')
@@ -9,25 +8,25 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
-    return res.sendFile(path.join(__dirname,'index.html'));
+    return res.sendFile(path.join(process.cwd(),'index.html'));
 })
 
 app.get('/image/:file', (req, res) => {
-    return res.sendFile(path.join(__dirname,`uploads/${req.params.file}`));
+    return res.sendFile(path.join(process.cwd(),`uploads/${req.params.file}`));
 })
 
 app.get('/all', (req, res) => {
-    let dir = fs.readdirSync('uploads')
+    let dir = fs.readdirSync(path.join(process.cwd(),`uploads`))
     return res.json(dir)
 })
 
 app.post('/', function (req, res) {
-    let base64String = JSON.stringify(req.body)
+    let base64String = JSON.strinigify(req.body)
     let base64Image = base64String.split(';base64,').pop();
-    fs.writeFile(`uploads/${uuidv4()}.jpeg`, base64Image, {encoding: 'base64'}, function(err) {});
+    fs.writeFile(path.join(process.cwd(),`uploads/${uuidv4()}.jpeg`), base64Image, {encoding: 'base64'}, function(err) {});
     return res.sendStatus(200)
 })
-
+const port = process.env.PORT | 3000
 app.listen(port, () => {
   console.log(`http://127.0.0.1:${port}`)
 })
